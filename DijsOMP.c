d86a75194a39bd523021cc7974f23d366916206b
MiguelIbrahimE <121685654+MiguelIbrahimE@users.noreply.github.com>
2023-05-03 21:51:58 +0300

    Add files via upload

--- DijsOMP.c
+++ DijsOMP.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <time.h>
 #include <omp.h>
 
-#define V 5
-int graph[V][V];
+#define V 1000
 
 int minDistance(int dist[], int sptSet[])
 {
-    int min = INT_MAX, min_index;
+    int min = INT_MAX, min_index = -1;
 
-#pragma omp parallel for
+    #pragma omp parallel for shared(dist, sptSet) reduction(min:min)
     for (int v = 0; v < V; v++)
+    {
         if (sptSet[v] == 0 && dist[v] <= min)
-#pragma omp critical
         {
-            if (dist[v] <= min)
-            {
-                min = dist[v];
-                min_index = v;
-            }
+            min = dist[v];
+            min_index = v;
         }
+    }
 
     return min_index;
 }
@@ -29,37 +25,40 @@ int minDistance(int dist[], int sptSet[])
 void printSolution(int dist[])
 {
     printf("Vertex \t Distance from Source\n");
+    #pragma omp parallel for shared(dist)
     for (int i = 0; i < V; i++)
         printf("%d \t %d\n", i, dist[i]);
 }
 
-void dijkstra(int src)
+void dijkstra(int graph[V][V], int src)
 {
-    int dist[V], sptSet[V];
+    int dist[V];
+    int sptSet[V];
 
-#pragma omp parallel for
+    #pragma omp parallel for shared(dist, sptSet)
     for (int i = 0; i < V; i++)
-        dist[i] = INT_MAX, sptSet[i] = 0;
+    {
+        dist[i] = INT_MAX;
+        sptSet[i] = 0;
+    }
 
     dist[src] = 0;
 
-#pragma omp parallel for
-    for (int count = 0; count < V - 1; count++) {
+    #pragma omp parallel for shared(dist, sptSet, graph)
+    for (int count = 0; count < V-1; count++)
+    {
         int u = minDistance(dist, sptSet);
 
         sptSet[u] = 1;
 
-#pragma omp parallel for
+        #pragma omp parallel for shared(dist, sptSet, graph)
         for (int v = 0; v < V; v++)
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-                && dist[u] + graph[u][v] < dist[v])
-#pragma omp critical
-                {
-                    if (dist[u] + graph[u][v] < dist[v])
-                    {
-                        dist[v] = dist[u] + graph[u][v];
-                    }
-                }
+        {
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+            {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
     }
 
     printSolution(dist);
@@ -67,10 +66,12 @@ void dijkstra(int src)
 
 int main()
 {
-    double start_time = omp_get_wtime();
-    // Generate random graph with 1000 vertices and 10000 edges
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
+    // Generate random graph with 5000 vertices and random edge weights
+    int graph[V][V];
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
             if (i == j)
                 graph[i][j] = 0;
             else if (rand() % 10 == 0)
@@ -81,10 +82,10 @@ int main()
     }
 
     // Measure the start time
-    
+    double start_time = omp_get_wtime();
 
     // Run Dijkstra's algorithm for vertex 0
-    dijkstra(0);
+    dijkstra(graph, 0);
 
     // Measure the end time
     double end_time = omp_get_wtime();
